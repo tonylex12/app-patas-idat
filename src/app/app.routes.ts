9b20a0d32508996684152e32156be7676a572eca
Tony HUerto <tonylex12@gmail.com>
2024-12-06 13:15:38 -0500

    outlet corregido

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './dashboard/home/home.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
     path: 'dashboard',
     component: DashboardComponent,
-    canActivate: [authGuard],
-    children: [
-      { path: 'home', component: HomeComponent },
-      { path: 'curso', component: CursolistaComponent },
-      { path: 'alumno', component: AlumnolistaComponent },
-      { path: 'alumno/:id', component: AlumnodetalleComponent },
-      { path: 'curso/:nombre', component: CursodetalleComponent },
-    ],
+    children: [{ path: 'home', component: HomeComponent }],
   },
 ];
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
-
-}
+export class DashboardComponent {}
