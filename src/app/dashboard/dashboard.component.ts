d6bdf137c33c92f479de536085a64e49fa1c965c
Tony HUerto <tonylex12@gmail.com>
2024-12-06 13:34:13 -0500

    modulo angular material añadido

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MaterialModule } from '../material/material/material.module';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, MaterialModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
