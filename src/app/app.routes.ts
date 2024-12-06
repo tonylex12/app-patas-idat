f16be95611dfb8db306ccd5b70f318373b39c5c1
Tony HUerto <tonylex12@gmail.com>
2024-12-06 13:08:22 -0500

    rutas añadidas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [authGuard],
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'curso', component: CursolistaComponent },
+      { path: 'alumno', component: AlumnolistaComponent },
+      { path: 'alumno/:id', component: AlumnodetalleComponent },
+      { path: 'curso/:nombre', component: CursodetalleComponent },
+    ],
+  },
+];
